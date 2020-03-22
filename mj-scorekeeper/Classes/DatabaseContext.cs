84b2924571b33df77ffac21ff77e447895ec44c9
Willy W <will.wong90@gmail.com>
2020-03-21 21:34:57 -0700

    Rename variable

--- mj-scorekeeper/Classes/DatabaseContext.cs
+++ mj-scorekeeper/Classes/DatabaseContext.cs
@@ -8,7 +8,7 @@ namespace mj_scorekeeper.Classes
 {
     public class DatabaseContext : DbContext
     {
-        public DbSet<PointConversion> Scoring { get; set; }
+        public DbSet<PointConversion> PointConversion { get; set; }
         public DbSet<Game> Games { get; set; }
         public DbSet<Score> Hands { get; set; }
 
@@ -19,12 +19,12 @@ namespace mj_scorekeeper.Classes
 
         public void addScoringValues()
         {
-            Scoring.Add(new PointConversion { Fan = 3, Points = 32, PointsSelfDrawn = 16});
-            Scoring.Add(new PointConversion { Fan = 4, Points = 64, PointsSelfDrawn = 32 });
-            Scoring.Add(new PointConversion { Fan = 5, Points = 96, PointsSelfDrawn = 48 });
-            Scoring.Add(new PointConversion { Fan = 6, Points = 128, PointsSelfDrawn = 64 });
-            Scoring.Add(new PointConversion { Fan = 7, Points = 192, PointsSelfDrawn = 96 });
-            Scoring.Add(new PointConversion { Fan = 8, Points = 256, PointsSelfDrawn = 128 });
+            PointConversion.Add(new PointConversion { Fan = 3, Points = 32, PointsSelfDrawn = 16});
+            PointConversion.Add(new PointConversion { Fan = 4, Points = 64, PointsSelfDrawn = 32 });
+            PointConversion.Add(new PointConversion { Fan = 5, Points = 96, PointsSelfDrawn = 48 });
+            PointConversion.Add(new PointConversion { Fan = 6, Points = 128, PointsSelfDrawn = 64 });
+            PointConversion.Add(new PointConversion { Fan = 7, Points = 192, PointsSelfDrawn = 96 });
+            PointConversion.Add(new PointConversion { Fan = 8, Points = 256, PointsSelfDrawn = 128 });
         }
     }
 }

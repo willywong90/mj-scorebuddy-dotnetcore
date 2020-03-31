809acbcab4a1a70ae90a5459539fcd8776ccbf0f
Willy W <will.wong90@gmail.com>
2020-03-31 16:27:51 -0700

    Add endpoint to retrieve current game scores.

--- mj-scorekeeper/Controllers/ApiController.cs
+++ mj-scorekeeper/Controllers/ApiController.cs
@@ -59,6 +59,41 @@ namespace mj_scorekeeper.Controllers
             return NotFound();
         }
 
+        [HttpGet]
+        [Route("game/{gameId}/score")]
+        public IActionResult score(string gameId)
+        {
+            Game game = dbContext.Games.Include(s => s.Hands).Where(s => s.GameId == gameId.ToUpper()).FirstOrDefault();
+
+            if (game != null)
+            {
+                Dictionary<string, int> scoreValuePairs = new Dictionary<string, int>();
+                scoreValuePairs.Add(game.Player1, 0);
+                scoreValuePairs.Add(game.Player2, 0);
+                scoreValuePairs.Add(game.Player3, 0);
+                scoreValuePairs.Add(game.Player4, 0);
+
+                var wins = game.Hands.GroupBy(s => s.Winner).Select(x => new { Winner = x.Key, Total = x.Sum(s => s.Points) });
+                var losses = game.Hands.GroupBy(s => s.Loser).Select(x => new { Loser = x.Key, Total = x.Sum(s => s.Points) });
+
+                foreach (var player in scoreValuePairs.Keys.ToList())
+                {
+                    var win = wins.FirstOrDefault(s => s.Winner == player);
+                    var loss = losses.FirstOrDefault(s => s.Loser == player);
+                    int totalWin = win != null ? win.Total : 0;
+                    int totalLoss = loss != null ? loss.Total : 0;
+
+                    totalLoss = totalLoss + game.Hands.Where(s => s.IsSelfDrawn == true && s.Winner != player).Sum(s => s.Points) / 3;
+
+                    scoreValuePairs[player] = totalWin - totalLoss;
+                }
+
+                return Ok(scoreValuePairs.OrderByDescending(s => s.Value));
+            }
+
+            return NotFound();
+        }
+
         [HttpPost]
         [Route("game/{gameId}/score/add")]
         public IActionResult addScore(string gameId, [FromBody] Score hand)
